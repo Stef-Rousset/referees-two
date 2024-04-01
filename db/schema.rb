1649f5fddd2318903d47d2a77bf65ef5d9bc7ac7
stephanie rousset <strabon@club-internet.fr>
2024-04-01 16:13:40 +0200

    add question model

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_04_01_072412) do
+ActiveRecord::Schema[7.0].define(version: 2024_04_01_140717) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "questions", force: :cascade do |t|
+    t.text "statement", null: false
+    t.text "prop_one", null: false
+    t.text "prop_two", null: false
+    t.text "prop_three", null: false
+    t.integer "level", null: false
+    t.integer "category", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_questions_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -26,4 +39,5 @@ ActiveRecord::Schema[7.0].define(version: 2024_04_01_072412) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "questions", "users"
 end
