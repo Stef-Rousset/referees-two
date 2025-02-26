a67d299bba771acb8a5b36fb0291aed338ff380d
stephanie rousset <strabon@club-internet.fr>
2025-02-26 16:12:47 +0100

    feat: add i18n

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  before_action :set_locale_from_params
   include Pundit::Authorization
 
   # Pundit: allow-list approach
@@ -10,8 +11,26 @@ class ApplicationController < ActionController::Base
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
 
   def user_not_authorized
-    flash[:alert] = "You are not authorized to perform this action."
+    flash[:alert] = "Vous n'êtes pas autorisé à réaliser cette action."
     redirect_to(root_path)
+  end
+
+  def default_url_options
+    { host: ENV['DOMAIN'] || 'localhost:3000', locale: I18n.locale }
+  end
+
+  protected
+
+  def set_locale_from_params
+    if params[:locale]
+      if I18n.available_locales.map(&:to_s).include?(params[:locale])
+        I18n.locale = params[:locale]
+      else
+        flash.now[:notice] = "La traduction en #{params[:locale]} n'est pas disponible"
+      end
+    else
+      I18n.locale = I18n.default_locale
+    end
   end
 
   private
