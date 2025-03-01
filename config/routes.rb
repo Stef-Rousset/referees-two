c10636c893b3cbcc1ff4a3df86b9ac69f23ebe28
stephanie rousset <strabon@club-internet.fr>
2025-03-01 15:09:54 +0100

    feat: add mailer config in dev and prod

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   scope '(:locale)', locale: /fr|en/ do
     devise_for :users
+
+    mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
     # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
     # Defines the root path route ("/")
