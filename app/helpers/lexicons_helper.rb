6a515f5066650761b7d611e0a38d046d5c7a5e1d
stephanie rousset <strabon@club-internet.fr>
2025-04-13 15:53:30 +0200

    feat: add helpers

--- app/helpers/lexicons_helper.rb
+++ app/helpers/lexicons_helper.rb
@@ -1,2 +1,18 @@
 module LexiconsHelper
+  def month_beginning
+    DateTime.now.day >= 1 && DateTime.now.day < 16 ? true : false
+  end
+
+  def l_category(lexicon)
+    case lexicon.category
+    when "rules"
+      "Règles"
+    when "actions"
+      "Actions"
+    when "prep_hab"
+      "Préparations et habiletés"
+    when "other"
+      "Autre"
+    end
+  end
 end
