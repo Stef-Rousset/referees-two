10c4f17b98d532ad1391c31e1a74b452fc332590
stephanie rousset <strabon@club-internet.fr>
2025-05-18 15:52:36 +0200

    feat: update methods in lexicon result model

--- app/models/lexicon_result.rb
+++ app/models/lexicon_result.rb
@@ -3,7 +3,12 @@ class LexiconResult < ApplicationRecord
 
   validates :score, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 20 }
 
-  def self.score(email, month)
-    joins(:user).where(users: { email: email }).where('extract(month from lexicon_results.created_at) = ?', month)&.first&.score
+  def self.results(email)
+    joins(:user).where(users: { email: email })
+  end
+
+  # méthode appelée sur une active record relation de LexiconResults d'un user
+  def self.month_score(month)
+    where('extract(month from lexicon_results.created_at) = ?', month)&.first&.score
   end
 end
