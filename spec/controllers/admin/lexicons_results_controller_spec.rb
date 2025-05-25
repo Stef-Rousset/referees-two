4335205afbc4c213e2c916f22c52551db0b30bab
stephanie rousset <strabon@club-internet.fr>
2025-05-25 15:41:39 +0200

    test: update lexicon result controller test

--- spec/controllers/admin/lexicons_results_controller_spec.rb
+++ spec/controllers/admin/lexicons_results_controller_spec.rb
@@ -20,8 +20,13 @@ RSpec.describe Admin::LexiconsResultsController, type: :controller do
       sign_in normal
     end
 
-    it 'should not access index' do
-      get :index
+    it 'should not access index with format html' do
+      get :index, format: :html
+      expect(flash[:alert]).to match("Vous n'êtes pas autorisé à réaliser cette action.")
+    end
+
+    it 'should not access index with format xlsx' do
+      get :index, format: :xlsx
       expect(flash[:alert]).to match("Vous n'êtes pas autorisé à réaliser cette action.")
     end
   end
@@ -31,8 +36,13 @@ RSpec.describe Admin::LexiconsResultsController, type: :controller do
       sign_in intern
     end
 
-    it 'should not access index' do
-      get :index
+    it 'should not access index with format html' do
+      get :index, format: :html
+      expect(flash[:alert]).to match("Vous n'êtes pas autorisé à réaliser cette action.")
+    end
+
+    it 'should not access index with format xlsx' do
+      get :index, format: :xlsx
       expect(flash[:alert]).to match("Vous n'êtes pas autorisé à réaliser cette action.")
     end
   end
@@ -42,8 +52,13 @@ RSpec.describe Admin::LexiconsResultsController, type: :controller do
       sign_in user
     end
 
-    it 'should access index page' do
-      get :index
+    it 'should access index page with format html' do
+      get :index, format: :html
+      assert_response :success
+    end
+
+    it 'should access index page with format xlsx' do
+      get :index, format: :xlsx
       assert_response :success
     end
   end
@@ -53,8 +68,13 @@ RSpec.describe Admin::LexiconsResultsController, type: :controller do
       sign_in contributor
     end
 
-    it 'should access index page' do
-      get :index
+    it 'should access index page with format html' do
+      get :index, format: :html
+      assert_response :success
+    end
+
+    it 'should access index page with format xlsx' do
+      get :index, format: :xlsx
       assert_response :success
     end
   end
